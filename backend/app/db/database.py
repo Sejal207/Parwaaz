6e7cf55a1f74ba534f745c79c275329dc944a6b3
Sejal207 <guptasejal1212@gmail.com>
2026-06-14 02:15:45 +0530

    Fix API routes

--- backend/app/db/database.py
+++ backend/app/db/database.py
@@ -1,9 +1,16 @@
+import os
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from app.core.config import settings
 
-engine = create_engine(settings.DATABASE_URL)
+DATABASE_URL = os.getenv("DATABASE_URL")
+
+engine = create_engine(
+    DATABASE_URL,
+    pool_pre_ping=True,
+    pool_recycle=300,
+)
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
