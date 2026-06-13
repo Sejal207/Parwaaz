2f5a725c21d4a3239bf38f538656d75ffa088370
Sejal207 <guptasejal1212@gmail.com>
2026-06-14 01:11:45 +0530

    Configure production API URL

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,8 +1,7 @@
 import axios from 'axios'
 
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_URL 
-})
+  baseURL: import.meta.env.VITE_API_URL })
 
 export const uploadSession = (formData, onProgress) =>
   api.post('/sessions/upload', formData, {
